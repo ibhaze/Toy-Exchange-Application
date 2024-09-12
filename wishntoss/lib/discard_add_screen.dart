88189de3228f47b3b479fc53546b1e846845b68e
memymini <minhi0614@gmail.com>
2024-09-12 13:09:49 +0200

    [feat]discard list & add

--- wishntoss/lib/discard_add_screen.dart
+++ wishntoss/lib/discard_add_screen.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 //Discard Add Screen
-class DiscardScreen extends StatefulWidget {
-  const DiscardScreen({super.key});
+class DiscardAddScreen extends StatefulWidget {
+  const DiscardAddScreen({super.key});
 
   @override
-  _DiscardScreenState createState() => _DiscardScreenState();
+  _DiscardAddScreenState createState() => _DiscardAddScreenState();
 }
 
-class _DiscardScreenState extends State<DiscardScreen> {
+class _DiscardAddScreenState extends State<DiscardAddScreen> {
   final ImagePicker _picker = ImagePicker();
   XFile? _image;
 
@@ -34,35 +34,117 @@ class _DiscardScreenState extends State<DiscardScreen> {
     }
   }
 
-  @override
+  // manage photo upload option
+  void _showUploadOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('Take Photo'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  _takePhoto();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Choose from Gallery'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  _pickImageFromGallery();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+   @override
   Widget build(BuildContext context) {
+    // 1번 박스 이후에 나타낼 텍스트 리스트
+    final List<String> uploadList = [
+      'Title',
+      'Condition',
+      'Categories',
+      'Notes'
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Discard Screen'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _image == null
-                ? const Text('No image selected.')
-                : Image.file(
-                    File(_image!.path),
-                    width: 300,
-                    height: 300,
+      body: Column(
+        children: List.generate(5, (index) {
+          if (index == 0) {
+            return GestureDetector(
+              // show photo upload option
+              onTap: () {
+                _showUploadOptions(context);
+              },
+              child: Container(
+                height: 200,
+                margin: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 169, 169, 169),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Center(
+                  child: _image != null
+                      ? ClipRRect(
+                          child: Image.file(
+                            File(_image!.path),
+                            width: 150,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.upload,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Upload Photo',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                ),
+              ),
+            );
+          } else {
+            return Container(
+              height: 50,
+              margin: const EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 189, 189, 189),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  uploadList[index - 1], 
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
                   ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _takePhoto,
-              child: const Text('Take Photo with Camera'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickImageFromGallery,
-              child: const Text('Pick Image from Gallery'),
-            ),
-          ],
-        ),
+                ),
+              ),
+            );
+          }
+        }),
       ),
     );
   }
--- wishntoss/lib/discard_list_screen.dart
+++ wishntoss/lib/discard_list_screen.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'discard_add_screen.dart';
 
 class DiscardListScreen extends StatelessWidget {
   const DiscardListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<String> discardedItems = [
-      'Toy 1',
-      'Toy 2',
-      'Toy 3',
-      'Toy 4',
-      'Toy 5',
-      'Toy 6',
-      'Toy 7',
-      'Toy 8',
-      'Toy 9',
-      'Toy 10',
-    ];
+    final Map<String, String> discardedItems = {
+      'Toy 1':'assets/images/toy1.png',
+      'Toy 2':'assets/images/toy2.png',
+      'Toy 3':'assets/images/toy3.png',
+      'Toy 4':'assets/images/toy3.png',
+      'Toy 5':'assets/images/toy2.png',
+      'Toy 6':'assets/images/toy2.png',
+      'Toy 7':'assets/images/toy1.png',
+    };
+
 
     return Scaffold(
       appBar: AppBar(
@@ -29,21 +28,51 @@ class DiscardListScreen extends StatelessWidget {
             crossAxisCount: 3, 
             crossAxisSpacing: 15.0, 
             mainAxisSpacing: 15.0, 
-            childAspectRatio: 3/4, 
+            childAspectRatio: 3 / 4, 
           ),
-          itemCount: discardedItems.length,
+          itemCount: discardedItems.length+1,
           itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  discardedItems[index],
-                  style: const TextStyle(color: Colors.white, fontSize: 16),
+            if (index == discardedItems.length) {
+              return GestureDetector(
+                onTap: () {
+                  // Move to DiscardAddScreen
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const DiscardAddScreen(),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 179, 179, 179), // 추가 버튼 배경색
+                  ),
+                  child: const Icon(
+                    Icons.add, // 추가 버튼 아이콘
+                    size: 40,
+                    color: Colors.white,
+                  ),
                 ),
+              );
+            } else {
+            String toyImage = discardedItems.values.elementAt(index);
+            return Container(
+              decoration: const BoxDecoration(
+                color:  Color.fromARGB(255, 177, 177, 177),
               ),
-            );
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        toyImage,
+                        fit: BoxFit.cover, // 이미지를 그리드 크기에 맞추고 잘라냄
+                        width: double.infinity,
+                      ),
+                    ),
+                  ],
+              ),
+            );}
           },
         ),
       ),
