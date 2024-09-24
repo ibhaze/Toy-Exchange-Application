0ed3e95049008f8e793842ef6fafff33c2e34ec6
memymini <minhi0614@gmail.com>
2024-09-24 17:51:00 +0200

    [feat]added design on profile page.

--- wishntoss/lib/screens/profile_screen.dart
+++ wishntoss/lib/screens/profile_screen.dart
@@ -63,55 +63,85 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20.0),
         child: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Text("Profile",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              SizedBox(height: 30),
               Center(
                 child: GestureDetector(
                   onTap: _pickImage,
                   child: CircleAvatar(
                     radius: 50,
-                    backgroundColor: Colors.grey[300],
+                    backgroundColor: Color(0xFFD9D9D9),
                     backgroundImage: _image != null ? FileImage(_image!) : null,
                     child: _image == null
                         ? const Icon(
                             Icons.camera_alt,
                             size: 40,
-                            color: Colors.white,
+                            color: Color(0xFFB22222),
                           )
                         : null,
                   ),
                 ),
               ),
+              const SizedBox(height: 30),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Name",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  _buildTextField(
+                    hintText: 'Name',
+                    controller: _usernameController,
+                    icon: Icons.person,
+                  ),
+                ],
+              ),
               const SizedBox(height: 20),
-              _buildTextField(
-                labelText: 'Name',
-                controller: _usernameController,
-                icon: Icons.person,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Email",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  _buildTextField(
+                    hintText: 'Email',
+                    controller: _emailController,
+                    icon: Icons.email,
+                    inputType: TextInputType.datetime,
+                  ),
+                ],
               ),
-              const SizedBox(height: 10),
-              _buildTextField(
-                labelText: 'Email',
-                controller: _emailController,
-                icon: Icons.email,
-                inputType: TextInputType.datetime,
-              ),
-              const SizedBox(height: 10),
-              _buildTextField(
-                labelText: 'Bio',
-                controller: _bioController,
-                icon: Icons.edit,
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Bio",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  _buildTextField(
+                    hintText: 'Bio',
+                    controller: _bioController,
+                    icon: Icons.edit,
+                  ),
+                ],
               ),
               const SizedBox(height: 20),
               Center(
                 child: ElevatedButton(
                   onPressed: _saveProfile,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color(0xFFB22222)), // 배경색 설정
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Colors.white), // 텍스트 색상 설정
+                  ),
                   child: const Text('Save Profile'),
                 ),
               ),
@@ -124,7 +154,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   // TextField builder
   Widget _buildTextField({
-    required String labelText,
+    required String hintText,
     required TextEditingController controller,
     required IconData icon,
     TextInputType inputType = TextInputType.text,
@@ -133,9 +163,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
       controller: controller,
       keyboardType: inputType,
       decoration: InputDecoration(
-        labelText: labelText,
-        prefixIcon: Icon(icon),
-        border: const OutlineInputBorder(),
+        hintText: hintText,
+        prefixIcon: Icon(icon, color: Color(0xFFB22222)),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25), // 모서리를 둥글게 설정
+          borderSide: BorderSide.none, // 테두리 없애기
+        ),
+        fillColor: Color(0xFFD9D9D9),
+        filled: true,
       ),
     );
   }
