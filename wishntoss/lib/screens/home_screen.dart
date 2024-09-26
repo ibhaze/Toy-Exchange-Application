28ef1225e68c6fc6c0adab078e0d3e298fa15d10
memymini <minhi0614@gmail.com>
2024-09-26 10:57:07 +0200

    [fix]modified SdkVersion for android test.

--- wishntoss/lib/screens/home_screen.dart
+++ wishntoss/lib/screens/home_screen.dart
@@ -30,108 +30,113 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        height: double.maxFinite,
         child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Container(
-                  child: Row(
+            padding: EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Image.asset("assets/images/sinterklaas_face.png",
-                      width: 48, height: 48),
-                  SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Container(
+                      child: Row(
                     children: [
-                      Text("Hello",
-                          style: TextStyle(fontSize: 14, color: Colors.grey)),
-                      Text(_userName,
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold))
+                      Image.asset("assets/images/sinterklaas_face.png",
+                          width: 48, height: 48),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Hello",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey)),
+                          Text(_userName,
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold))
+                        ],
+                      )
                     ],
-                  )
-                ],
-              )),
-              SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFFD9D9D9),
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search',
-                    hintStyle: TextStyle(
-                      color: Color(0x66CC0036),
-                      fontSize: 16,
+                  )),
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFD9D9D9),
+                      borderRadius: BorderRadius.circular(50.0),
                     ),
-                    prefixIcon: Icon(Icons.search, color: Color(0xFFCC0036)),
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.all(15.0),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        hintStyle: TextStyle(
+                          color: Color(0x66CC0036),
+                          fontSize: 16,
+                        ),
+                        prefixIcon:
+                            Icon(Icons.search, color: Color(0xFFCC0036)),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(15.0),
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(height: 20),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(horizontal: 16.0),
+                            height: 30,
+                            decoration: BoxDecoration(
+                              color: Color(0x22CC0036),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: Text("Stuffed",
+                                style: TextStyle(
+                                    color: Color(0xFFCC0036),
+                                    fontWeight: FontWeight.bold))),
+                        SizedBox(width: 10),
+                        Container(
+                            alignment: Alignment.center,
+                            height: 30,
+                            padding: EdgeInsets.symmetric(horizontal: 16.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Color(0x0000000)),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: Text("Board Games")),
+                        SizedBox(width: 10),
+                        Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(horizontal: 16.0),
+                            height: 30,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Color(0x0000000)),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Text("Educational")),
+                        SizedBox(width: 10),
+                        Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(horizontal: 16.0),
+                            height: 30,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Color(0x0000000)),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Text("Stuffed")),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                      width: double.infinity,
+                      alignment: Alignment.centerLeft,
+                      child: Text("Recommended",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25))),
+                  SizedBox(height: 20),
+                  RecommendedWidget(),
+                ],
               ),
-              SizedBox(height: 20),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(horizontal: 16.0),
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: Color(0x22CC0036),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: Text("Stuffed",
-                            style: TextStyle(
-                                color: Color(0xFFCC0036),
-                                fontWeight: FontWeight.bold))),
-                    SizedBox(width: 10),
-                    Container(
-                        alignment: Alignment.center,
-                        height: 30,
-                        padding: EdgeInsets.symmetric(horizontal: 16.0),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Color(0x0000000)),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: Text("Board Games")),
-                    SizedBox(width: 10),
-                    Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(horizontal: 16.0),
-                        height: 30,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Color(0x0000000)),
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Text("Educational")),
-                    SizedBox(width: 10),
-                    Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(horizontal: 16.0),
-                        height: 30,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Color(0x0000000)),
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Text("Stuffed")),
-                  ],
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                  width: double.infinity,
-                  alignment: Alignment.centerLeft,
-                  child: Text("Recommended",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 25))),
-              SizedBox(height: 20),
-              RecommendedWidget(),
-            ],
-          ),
-        ),
+            )),
       ),
     );
   }
